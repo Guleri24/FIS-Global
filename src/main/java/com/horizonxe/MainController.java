a5fb256ddaa39c51183b8db893efc6be4a8e87cb
Guleri24 <guleri24@tuta.io>
2022-03-13 17:06:51 +0530

    Added import data facility

--- src/main/java/com/horizonxe/MainController.java
+++ src/main/java/com/horizonxe/MainController.java
@@ -1,5 +1,6 @@
 package com.horizonxe;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -11,12 +12,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class MainController {
     private static BufferedWriter writer;
-    private final List<String> projects = new ArrayList<>();
-    private final List<String> modules = new ArrayList<>();
     @FXML
     public Button triggerCmd;
     @FXML
@@ -24,32 +22,48 @@ public class MainController {
     @FXML
     public ComboBox<String> moduleMenu;
     public AnchorPane mainView;
-    private File file;
+    private final List<String> projects = new ArrayList<>();
+    private final List<String> modules = new ArrayList<>();
+    private File executableFile;
+    private File dataFile;
     private boolean isWindows;
 
     public void initialize() throws IOException, InterruptedException {
-        file = newFile();
-        file.getParentFile().mkdir();
-        file.createNewFile();
-        file.setExecutable(true);
-        writer = Files.newBufferedWriter(Paths.get(String.valueOf(file)));
+        newFile("exec");
+        writer = Files.newBufferedWriter(Paths.get(String.valueOf(executableFile)));
         writer.write("");
         writer.flush();
         initData();
-        projectMenu.getItems().addAll(projects);
-        projectMenu.setValue("");  // empty selection is object and not null
-        moduleMenu.getItems().addAll(modules);
     }
 
-    private File newFile() throws IOException, InterruptedException {
+    private void newFile(String type) throws IOException {
         isWindows = System.getProperty("os.name")
                 .toLowerCase().startsWith("windows");
         String username = System.getProperty("user.name");
+        String windowsPath = "C:\\Users\\" + username + "\\FisGlobal\\";
+        String linuxPath = "/home/" + username + "/FisGlobal/";
+        switch (type) {
+            case "exec" -> {
+                if (isWindows) {
+                    executableFile = new File(windowsPath + "exec.bat");
+                } else {
+                    executableFile = new File(linuxPath + "exec.sh");
+                }
+                executableFile.getParentFile().mkdir();
+                executableFile.createNewFile();
+                executableFile.setExecutable(true);
+            }
+            case "data" -> {
+                if (isWindows) {
+                    dataFile = new File(windowsPath + "data.csv");
+                } else {
+                    dataFile = new File(linuxPath + "data.csv");
+                }
 
-        if (isWindows) {
-            return new File("C:\\Users\\" + username + "\\FisGlobal\\data.bat");
-        } else {
-            return file = new File("/home/" + username + "/FisGlobal/data.sh");
+                dataFile.getParentFile().mkdir();
+                dataFile.createNewFile();
+                dataFile.setExecutable(true);
+            }
         }
     }
 
@@ -63,7 +77,6 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     private void writeData(BufferedWriter writer, String project, String module) throws IOException {
@@ -71,18 +84,20 @@ public class MainController {
     }
 
     // Enter the projects and modules here
-    private void initData() {
-        projects.add("France");
-        projects.add("Germany");
-        projects.add("Switzerland");
+    private void initData() throws IOException {
+        newFile("data");
+        String row;
+        BufferedReader csvReader = new BufferedReader(new FileReader(dataFile));
+        while ((row = csvReader.readLine()) != null) {
 
-        modules.add("Paris");
-        modules.add("Strasbourg");
-        modules.add("Berlin");
-        modules.add("Cologne");
-        modules.add("Munich");
-        modules.add("Zurich");
+            String[] data = row.split(",");
+            projects.add(data[0]);
+            modules.add(data[1]);
+        }
+        csvReader.close();
 
+        projectMenu.setItems(FXCollections.observableArrayList(projects));
+        moduleMenu.setItems(FXCollections.observableArrayList(modules));
     }
 
     @FXML
@@ -92,16 +107,30 @@ public class MainController {
         writer.close();
 
         if (isWindows) {
-            Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"" + file.getAbsolutePath() + "\"");
+            Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"" + executableFile.getAbsolutePath() + "\"");
             stage.close();
         } else {
             stage.close();
         /*
 		    Runtime.getRuntime().exec(System.getenv("TERM") + " -e bash -c " + file.getAbsolutePath() + ";read");
         */
-            Runtime.getRuntime().exec("alacritty" + " -e bash -c " + file.getAbsolutePath() + ";read");
+            Runtime.getRuntime().exec("alacritty" + " -e bash -c " + executableFile.getAbsolutePath() + ";read");
         }
+    }
 
+    @FXML
+    public void addData() throws IOException {
+        Stage stage = (Stage) mainView.getScene().getWindow();
+        if (isWindows) {
+            Runtime.getRuntime().exec("cmd /c start excel /K \"" + dataFile.getAbsolutePath() + "\"");
+            stage.close();
+        } else {
+            stage.close();
+        /*
+		    Runtime.getRuntime().exec(System.getenv("TERM") + " -e bash -c " + file.getAbsolutePath() + ";read");
+        */
+            Runtime.getRuntime().exec("gio " + " open " + dataFile.getAbsolutePath());
+        }
     }
 }
 
